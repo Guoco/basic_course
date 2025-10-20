b90751db4a42b72c86a6e80171cb31cf51614ca7
GuoGuo <a0963622911@gmail.com>
2025-10-20 17:16:45 +0800

    Update project to target .NET Framework 4.8; refactor code and improve comments for clarity

--- Session_2_Operators/Exercise/Session2_Operator/Form1.cs
+++ Session_2_Operators/Exercise/Session2_Operator/Form1.cs
@@ -25,20 +25,34 @@ namespace Session2_Operator
         {
             //字串相加
             txtAnswerA.Text = txtAdd1.Text + txtAdd2.Text;
-            //.Trim()去除多餘空白
-            txtAnswerA.Text = txtAdd1.Text.Trim() + txtAdd2.Text.Trim();
-            //int.TryParse 將數字的字串表示轉換成它的對等 32 位元帶正負號的整數。 傳回指示轉換是否成功的值。
+            //介紹 .Trim()去除多餘空白
+            //txtAnswerA.Text = txtAdd1.Text.Trim() + txtAdd2.Text.Trim();
+
+            // 介紹 int.TryParse 將數字的字串表示轉換成它的對等 32 位元帶正負號的整數。 傳回指示轉換是否成功的值。
             int iAdd1;
             int iAdd2;
-            int.TryParse(txtAdd1.Text,out iAdd1);
+            int.TryParse(txtAdd1.Text, out iAdd1);
             int.TryParse(txtAdd2.Text, out iAdd2);
             //數字相加後放回畫面上控制項txtAnswerA 因TextBox是需要放入String 所以要 .ToStrint()
             txtAnswerA.Text = (iAdd1 + iAdd2).ToString();
+
             //Convert強制轉型態 如沒增加錯誤判斷會造成 系統例外錯誤導致程式關閉
-            txtAnswerA.Text = (Convert.ToInt16(txtAdd1.Text) + Convert.ToInt16(txtAdd2.Text)).ToString();
+            //         txtAnswerA.Text = (Convert.ToInt16(txtAdd1.Text) + Convert.ToInt16(txtAdd2.Text)).ToString();
+
+            //Convert 相關介紹
+            //| 型別 | 位元數(bit) | 位元組(byte) | 範圍                | 會佔的記憶體 |
+            //         | -------------------   | --------- | --------------------| --------------------|
+            //         | **Int16 / short       | 16 | 2    | -32,768 ~32,767     | 每個值佔 * *2 bytes |
+            //         | **UInt16 / ushort     | 16 | 2    | 0 ~65,535           | 每個值佔 * *2 bytes |
+            //         | **Int32 / int         | 32 | 4    | 約 - 21億 ~+21億    | 每個值佔 * *4 bytes |
+            //         | **UInt32 / uint       | 32 | 4    | 0 ~42億             | 每個值佔 * *4 bytes |
+            //         | **Int64 / long        | 64 | 8    | 約 ±9×10¹⁸          | 每個值佔 * *8 bytes |
+            //         | **UInt64 / ulong      | 64 | 8    | 0 ~1.8×10¹⁹         | 每個值佔 * *8 bytes |
+
+
         }
 
-        private void btnClearAdd_Click(object sender, EventArgs e)
+		private void btnClearAdd_Click(object sender, EventArgs e)
         {
             txtAdd1.Clear();
             txtAdd2.Text = "";
