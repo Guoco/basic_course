3e82231b2142021358ee8f34bc0ad0cae3b2a0df
GuoGuo <a0963622911@gmail.com>
2025-10-21 09:15:48 +0800

    修改md2以及內容

--- Session_2_Operators/Exercise/Session2_Operator/Form1.Designer.cs
+++ Session_2_Operators/Exercise/Session2_Operator/Form1.Designer.cs
@@ -36,6 +36,13 @@
 			this.txtAdd2 = new System.Windows.Forms.TextBox();
 			this.txtAdd1 = new System.Windows.Forms.TextBox();
 			this.btnCalculate = new System.Windows.Forms.Button();
+			this.btnCalculateSub = new System.Windows.Forms.Button();
+			this.btnClearSub = new System.Windows.Forms.Button();
+			this.txtAnswerSub = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtSub2 = new System.Windows.Forms.TextBox();
+			this.txtSub1 = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
 			// 
 			// label3
@@ -102,6 +109,7 @@
 			this.txtAdd2.Size = new System.Drawing.Size(76, 30);
 			this.txtAdd2.TabIndex = 10;
 			this.txtAdd2.TextChanged += new System.EventHandler(this.txtAdd2_TextChanged);
+			this.txtAdd2.Leave += new System.EventHandler(this.txtAdd2_Leave);
 			// 
 			// txtAdd1
 			// 
@@ -112,6 +120,7 @@
 			this.txtAdd1.Size = new System.Drawing.Size(76, 30);
 			this.txtAdd1.TabIndex = 9;
 			this.txtAdd1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAdd1_KeyPress);
+			this.txtAdd1.Leave += new System.EventHandler(this.txtAdd1_Leave);
 			// 
 			// btnCalculate
 			// 
@@ -125,11 +134,93 @@
 			this.btnCalculate.UseVisualStyleBackColor = true;
 			this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
 			// 
+			// btnCalculateSub
+			// 
+			this.btnCalculateSub.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnCalculateSub.Location = new System.Drawing.Point(649, 106);
+			this.btnCalculateSub.Margin = new System.Windows.Forms.Padding(2);
+			this.btnCalculateSub.Name = "btnCalculateSub";
+			this.btnCalculateSub.Size = new System.Drawing.Size(70, 30);
+			this.btnCalculateSub.TabIndex = 23;
+			this.btnCalculateSub.Text = "計算";
+			this.btnCalculateSub.UseVisualStyleBackColor = true;
+			this.btnCalculateSub.Click += new System.EventHandler(this.btnCalculateSub_Click);
+			// 
+			// btnClearSub
+			// 
+			this.btnClearSub.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.btnClearSub.Location = new System.Drawing.Point(79, 109);
+			this.btnClearSub.Margin = new System.Windows.Forms.Padding(2);
+			this.btnClearSub.Name = "btnClearSub";
+			this.btnClearSub.Size = new System.Drawing.Size(80, 31);
+			this.btnClearSub.TabIndex = 21;
+			this.btnClearSub.Text = "Clear";
+			this.btnClearSub.UseVisualStyleBackColor = true;
+			this.btnClearSub.Click += new System.EventHandler(this.btnClearSub_Click);
+			// 
+			// txtAnswerSub
+			// 
+			this.txtAnswerSub.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.txtAnswerSub.Location = new System.Drawing.Point(518, 109);
+			this.txtAnswerSub.Margin = new System.Windows.Forms.Padding(2);
+			this.txtAnswerSub.Name = "txtAnswerSub";
+			this.txtAnswerSub.Size = new System.Drawing.Size(76, 30);
+			this.txtAnswerSub.TabIndex = 22;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label4.Location = new System.Drawing.Point(453, 118);
+			this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(20, 19);
+			this.label4.TabIndex = 20;
+			this.label4.Text = "=";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.label5.Location = new System.Drawing.Point(311, 115);
+			this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(13, 16);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "-";
+			// 
+			// txtSub2
+			// 
+			this.txtSub2.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.txtSub2.Location = new System.Drawing.Point(342, 111);
+			this.txtSub2.Margin = new System.Windows.Forms.Padding(2);
+			this.txtSub2.Name = "txtSub2";
+			this.txtSub2.Size = new System.Drawing.Size(76, 30);
+			this.txtSub2.TabIndex = 18;
+			this.txtSub2.Leave += new System.EventHandler(this.txtSub2_Leave);
+			// 
+			// txtSub1
+			// 
+			this.txtSub1.Font = new System.Drawing.Font("標楷體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.txtSub1.Location = new System.Drawing.Point(223, 109);
+			this.txtSub1.Margin = new System.Windows.Forms.Padding(2);
+			this.txtSub1.Name = "txtSub1";
+			this.txtSub1.Size = new System.Drawing.Size(76, 30);
+			this.txtSub1.TabIndex = 17;
+			this.txtSub1.Leave += new System.EventHandler(this.txtSub1_Leave);
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(770, 336);
+			this.Controls.Add(this.btnCalculateSub);
+			this.Controls.Add(this.btnClearSub);
+			this.Controls.Add(this.txtAnswerSub);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtSub2);
+			this.Controls.Add(this.txtSub1);
 			this.Controls.Add(this.btnCalculate);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.btnClearAdd);
@@ -155,6 +246,13 @@
         private System.Windows.Forms.TextBox txtAdd2;
         private System.Windows.Forms.TextBox txtAdd1;
         private System.Windows.Forms.Button btnCalculate;
-    }
+		private System.Windows.Forms.Button btnCalculateSub;
+		private System.Windows.Forms.Button btnClearSub;
+		private System.Windows.Forms.TextBox txtAnswerSub;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtSub2;
+		private System.Windows.Forms.TextBox txtSub1;
+	}
 }
 
--- Session_2_Operators/Exercise/Session2_Operator/Form1.cs
+++ Session_2_Operators/Exercise/Session2_Operator/Form1.cs
@@ -23,34 +23,46 @@ namespace Session2_Operator
         //加法按鈕計算 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            //字串相加
-            txtAnswerA.Text = txtAdd1.Text + txtAdd2.Text;
-            //介紹 .Trim()去除多餘空白
-            //txtAnswerA.Text = txtAdd1.Text.Trim() + txtAdd2.Text.Trim();
+			//字串相加
+			txtAnswerA.Text = txtAdd1.Text + txtAdd2.Text;
+			//介紹 .Trim()去除多餘空白
+			txtAnswerA.Text = txtAdd1.Text.Trim() + txtAdd2.Text.Trim();
 
-            // 介紹 int.TryParse 將數字的字串表示轉換成它的對等 32 位元帶正負號的整數。 傳回指示轉換是否成功的值。
-            int iAdd1;
-            int iAdd2;
-            int.TryParse(txtAdd1.Text, out iAdd1);
-            int.TryParse(txtAdd2.Text, out iAdd2);
-            //數字相加後放回畫面上控制項txtAnswerA 因TextBox是需要放入String 所以要 .ToStrint()
-            txtAnswerA.Text = (iAdd1 + iAdd2).ToString();
+			// 介紹 int.TryParse 將數字的字串表示轉換成它的對等 32 位元帶正負號的整數。 傳回指示轉換是否成功的值。
 
-            //Convert強制轉型態 如沒增加錯誤判斷會造成 系統例外錯誤導致程式關閉
-            //         txtAnswerA.Text = (Convert.ToInt16(txtAdd1.Text) + Convert.ToInt16(txtAdd2.Text)).ToString();
+			int iAdd1;
+			int iAdd2;
 
-            //Convert 相關介紹
-            //| 型別 | 位元數(bit) | 位元組(byte) | 範圍                | 會佔的記憶體 |
-            //         | -------------------   | --------- | --------------------| --------------------|
-            //         | **Int16 / short       | 16 | 2    | -32,768 ~32,767     | 每個值佔 * *2 bytes |
-            //         | **UInt16 / ushort     | 16 | 2    | 0 ~65,535           | 每個值佔 * *2 bytes |
-            //         | **Int32 / int         | 32 | 4    | 約 - 21億 ~+21億    | 每個值佔 * *4 bytes |
-            //         | **UInt32 / uint       | 32 | 4    | 0 ~42億             | 每個值佔 * *4 bytes |
-            //         | **Int64 / long        | 64 | 8    | 約 ±9×10¹⁸          | 每個值佔 * *8 bytes |
-            //         | **UInt64 / ulong      | 64 | 8    | 0 ~1.8×10¹⁹         | 每個值佔 * *8 bytes |
+			int.TryParse(txtAdd1.Text, out iAdd1);
+			int.TryParse(txtAdd2.Text, out iAdd2);
+			//數字相加後放回畫面上控制項txtAnswerA 因TextBox是需要放入String 所以要 .ToStrint()
+			txtAnswerA.Text = (iAdd1 + iAdd2).ToString();
+
+			//Convert強制轉型態 如沒增加錯誤判斷會造成 系統例外錯誤導致程式關閉
+			try
+			{
+				txtAnswerA.Text = (Convert.ToInt16(txtAdd1.Text) + Convert.ToInt16(txtAdd2.Text)).ToString();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				Console.WriteLine("錯誤訊息：" + ex.Message);
+			}
+			//Convert強制轉型態 如沒增加錯誤判斷會造成 系統例外錯誤導致程式關閉
+			//         txtAnswerA.Text = (Convert.ToInt16(txtAdd1.Text) + Convert.ToInt16(txtAdd2.Text)).ToString();
+
+			//Convert 相關介紹
+			//| 型別 | 位元數(bit) | 位元組(byte) | 範圍                | 會佔的記憶體 |
+			//         | -------------------   | --------- | --------------------| --------------------|
+			//         | **Int16 / short       | 16 | 2    | -32,768 ~32,767     | 每個值佔 * *2 bytes |
+			//         | **UInt16 / ushort     | 16 | 2    | 0 ~65,535           | 每個值佔 * *2 bytes |
+			//         | **Int32 / int         | 32 | 4    | 約 - 21億 ~+21億    | 每個值佔 * *4 bytes |
+			//         | **UInt32 / uint       | 32 | 4    | 0 ~42億             | 每個值佔 * *4 bytes |
+			//         | **Int64 / long        | 64 | 8    | 約 ±9×10¹⁸          | 每個值佔 * *8 bytes |
+			//         | **UInt64 / ulong      | 64 | 8    | 0 ~1.8×10¹⁹         | 每個值佔 * *8 bytes |
 
 
-        }
+		}
 
 		private void btnClearAdd_Click(object sender, EventArgs e)
         {
@@ -64,23 +76,136 @@ namespace Session2_Operator
         //---------暫時先不看-------------------
         private void txtAdd1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            //Char.IsNumber 判斷書入是否為數字
-            //Char.IsControl 判斷是否為控制字元 ex: enter backspace
-            if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
-            {
-                e.Handled = true;//事件已處理--->這裡為消除非數字及控制項之字元
-            }
+
+			//---------進階判斷 只能輸入數字方法一--------------------
+            //因為鎖定只能輸入數字如果要移除(Backspace時)會無法輸入故判斷控制元件也能輸入
+			//Char.IsNumber 判斷書入是否為數字
+			//Char.IsControl 判斷是否為控制字元 ex: enter backspace
+			//if (!Char.IsNumber(e.KeyChar) && !Char.IsControl(e.KeyChar))
+   //         {
+   //             e.Handled = true;//事件已處理--->這裡為消除非數字及控制項之字元
+   //         }
 
         }
 
         private void txtAdd2_TextChanged(object sender, EventArgs e)
         {
             //Regex.IsMatch 運算式是否在輸入字串中尋找比對 是否符合條件
-            if (!Regex.IsMatch(txtAdd2.Text, @"^[0-9]*$"))
-            {
-                MessageBox.Show("請輸入數字");
-                txtAdd2.Text = "";
-			}
+            //---------進階判斷 只能輸入數字方法二--------------------
+
+   //         if (!Regex.IsMatch(txtAdd2.Text, @"^[0-9]*$"))
+   //         {
+   //             MessageBox.Show("請輸入數字");
+   //             txtAdd2.Text = "";
+			//}
         }
-    }
+		private void txtAdd1_Leave(object sender, EventArgs e)
+		{
+			if (txtAdd1.Text.Trim() != "")
+			{
+				int iAdd2;
+				if (int.TryParse(txtAdd1.Text, out iAdd2))
+				{
+					//doNothing
+				}
+				else
+				{
+					MessageBox.Show("你輸入的資料不是數字喔");
+					txtAdd1.Text = "";
+					txtAdd1.Focus();
+				}
+			}
+		}
+		private void txtAdd2_Leave(object sender, EventArgs e)
+		{
+			if (txtAdd2.Text.Trim() != "")
+			{
+				int iAdd2;
+				if (int.TryParse(txtAdd2.Text, out iAdd2))
+				{
+					//doNothing
+				}
+				else
+				{
+					MessageBox.Show("你輸入的資料不是數字喔");
+					txtAdd2.Text = "";
+					txtAdd2.Focus();
+				}
+			}
+		}
+
+		/// <summary>
+		/// 減法計算按鈕點擊事件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnCalculateSub_Click(object sender, EventArgs e)
+		{
+			//使用Conver.ToInt強制轉換如輸入非數字會發生例外錯誤 故請使用TryCatch
+			try
+			{
+				txtAnswerSub.Text = (Convert.ToInt16(txtSub1.Text) - Convert.ToInt16(txtSub2.Text)).ToString();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				//Console.WriteLine("錯誤訊息：" + ex.Message);
+			}
+		}
+		/// <summary>
+		/// 減法清空控制項(TextBox)按鈕事件
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnClearSub_Click(object sender, EventArgs e)
+		{
+			txtSub1.Text = "";
+			txtSub2.Text = "";
+			txtAnswerSub.Text = "";
+		}
+		/// <summary>
+		/// 減法txtSub1 Leave事件 離開控制項時判斷
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void txtSub1_Leave(object sender, EventArgs e)
+		{
+			if (txtSub1.Text.Trim() != "")
+			{
+				int iAdd2;
+				if (int.TryParse(txtSub1.Text, out iAdd2))
+				{
+					//doNothing
+				}
+				else
+				{
+					MessageBox.Show("你輸入的資料不是數字喔");
+					txtSub1.Text = "";
+					txtSub1.Focus();
+				}
+			}
+		}
+		/// <summary>
+		/// 減法txtSub2 Leave事件 離開控制項時判斷
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void txtSub2_Leave(object sender, EventArgs e)
+		{
+			if (txtSub2.Text.Trim() != "")
+			{
+				int iAdd2;
+				if (int.TryParse(txtSub2.Text, out iAdd2))
+				{
+					//doNothing
+				}
+				else
+				{
+					MessageBox.Show("你輸入的資料不是數字喔");
+					txtSub2.Text = "";
+					txtSub2.Focus();
+				}
+			}
+		}
+	}
 }
